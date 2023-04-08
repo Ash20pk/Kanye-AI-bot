0eb0022020b58a8cfdc03ba54529f21e04f6b917
Ash20pk <ash20pk@gmail.com>
2023-04-08 12:22:15 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function iMessage() {
     const newMessage = { user: true, text: userInput };
     setMessages([...messages, newMessage, { user: false, text: "loading" }]);
     setKanyeTyping(true);
-    axios.post('https://18.168.150.218:80/', {
+    axios.post('http://127.0.0.1:8000', {
         message: userInput
     }, {
         headers: {
